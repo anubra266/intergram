1a8974dd06670b641d4398ed36e7cada8f62c40c
Abraham <anubra266@gmail.com>
2020-08-15 22:01:02 +0100

    dw

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ app.post('/usage-end', cors(), function(req, res) {
     res.end();
 });
 
-http.listen(process.env.PORT || 3001, function(){
-    console.log('listening on port:' + (process.env.PORT || 3001));
+http.listen(process.env.PORT || 3000, function(){
+    console.log('listening on port:' + (process.env.PORT || 3000));
 });
 
 app.get("/.well-known/acme-challenge/:content", (req, res) => {
